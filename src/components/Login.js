982e3731afee29fffba326161f5720430d86ea49
Zied Snoussi <ziedsnoussi.tn@gmail.com>
2023-04-02 14:45:21 +0100

    add login and profile components

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <button className='btn1'>se connecter</button>
+    </div>
+  )
+}
+
+export default Login;
---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+        <ul>
+            <li>Email : </li>
+            <li>Nom : </li>
+            <li>Prenom : </li>
+            <li>Adress : </li>
+        </ul>
+    </div> 
+  )
+}
+
+export default Profile;
